d64deb53140ba5e6e3ee4b15de4691bdbe52ae00
Simon Criado <Simon@MacBook-Pro-de-Simon.local>
2019-05-30 12:56:54 +0200

    Created first table using js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
-var members = data.results[0].members;
-getMembersName(members);
-function getMembersName(arrayOfMembers) {
-    for (var i = 0; i < arrayOfMembers.length; i++) {
-        var lastName = arrayOfMembers[i].last_name;
-        var firstName = arrayOfMembers[i].first_name;
-        var middleName = arrayOfMembers[i].middle_name;
-        console.log(lastName, firstName, middleName);
+var arrayOfMembers = data.results[0].members;
+
+createTable(arrayOfMembers)
+
+// getMembersName(members);
+function getMembersName(member) {
+    var lastName = member.last_name;
+    var firstName = member.first_name;
+    var middleName = member.middle_name;
+    if (middleName == null) {
+        middleName = ""
     }
+    console.log(lastName, firstName, middleName);
+    // return lastName + ", " + firstName + " " + middleName;
+    return `${lastName}, ${firstName} ${middleName}`;
 }
 
-// getMembersParty(members);
-// function getMembersParty(arrayOfMembers) {
-//     for (var i = 0; i < arrayOfMembers.length; i++) {
-//         var party = arrayOfMembers[i].party;
-//         console.log(party);
-//     }
-// }
+// Creates the table of senators
+function createTable(members) {
+    var tbody = document.getElementById("table-body");
+
+    for (var i = 0; i < members.length; i++) {
+        var tr = document.createElement("tr");
+
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");
+        var td4 = document.createElement("td");
+        var td5 = document.createElement("td");
+
+        td1.innerHTML = getMembersName(members[i]);
+        td2.innerHTML = members[i].party;
+        td3.innerHTML = members[i].state;
+        td4.innerHTML = members[i].seniority;
+        td5.innerHTML = `${members[i].votes_with_party_pct}%`;
+        tr.append(td1, td2, td3, td4, td5);
+
+        tbody.append(tr);
+    }
+}
