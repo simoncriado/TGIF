66df52d63fcb67de32c9dad10b7aba8765a9921c
Simon Criado <Simon@MacBook-Pro-de-Simon.local>
2019-06-04 15:03:54 +0200

    Created style for text and table

--- js/statistics-object.js
+++ js/statistics-object.js
@@ -22,18 +22,23 @@ var statistics = {
 };
 var arrayOfMembers = data.results[0].members;
 
+// Sets total members by party
 statistics.democrats.numberOfMembers = numberOfMembers(arrayOfMembers, "D");
 statistics.republicans.numberOfMembers = numberOfMembers(arrayOfMembers, "R");
 statistics.independents.numberOfMembers = numberOfMembers(arrayOfMembers, "I");
+// Sets average votes by party
 statistics.democrats.averageVotes = (averageVotes(arrayOfMembers, "D") / statistics.democrats.numberOfMembers).toFixed(2);
 statistics.republicans.averageVotes = (averageVotes(arrayOfMembers, "R") / statistics.republicans.numberOfMembers).toFixed(2);
 statistics.independents.averageVotes = (averageVotes(arrayOfMembers, "I") / statistics.independents.numberOfMembers).toFixed(2);
+// Sets totals
 statistics.totals.numberOfMembers = arrayOfMembers.length;
 statistics.totals.averageVotes = ((Number(statistics.democrats.averageVotes) + Number(statistics.republicans.averageVotes) + Number(statistics.independents.averageVotes)) / 3).toFixed(2);
+// I will need to delete this console.log
 console.log(statistics);
 
 createTableGlance(statistics);
 
+// Gets the number of members based on each party
 function numberOfMembers(members, letter) {
     var numberOfMembersByParty = 0;
     for (var i = 0; i < members.length; i++) {
@@ -45,6 +50,7 @@ function numberOfMembers(members, letter) {
     return numberOfMembersByParty;
 }
 
+// Gets the average votes based on each party
 function averageVotes(members, letter) {
     var votesByParty = 0;
     for (var i = 0; i < members.length; i++) {
@@ -55,19 +61,20 @@ function averageVotes(members, letter) {
     return votesByParty;
 }
 
+// Created the table "glance" based on the results above
 function createTableGlance(object) {
     var tbody = document.getElementById("table-glance");
 
-    for (var keys in statistics) {
+    for (var keys in object) {
         var tr = document.createElement("tr");
 
         var td1 = document.createElement("td");
         var td2 = document.createElement("td");
         var td3 = document.createElement("td");
 
-        td1.innerHTML = statistics[keys].name;
-        td2.innerHTML = statistics[keys].numberOfMembers;
-        td3.innerHTML = statistics[keys].averageVotes;
+        td1.innerHTML = object[keys].name;
+        td2.innerHTML = object[keys].numberOfMembers;
+        td3.innerHTML = object[keys].averageVotes;
 
         tr.append(td1, td2, td3);
 
