3b7921994aac863b4fcfb66a037d84c3d3763ed8
Simon Criado <Simon@MacBook-Pro-de-Simon.local>
2019-06-04 12:40:29 +0200

    Created table senate/house at a glance

--- js/statistics-object.js
+++ js/statistics-object.js
@@ -1,17 +1,38 @@
 var statistics = {
-    numberOfDemocrats: 0, averageVotesDemocrats: 0,
-    numberOfRepublicans: 0, averageVotesRepublicans: 0,
-    numberOfIndependents: 0, averageVotesIndependents: 0,
+    democrats: {
+        name: "Democrats",
+        numberOfMembers: 0,
+        averageVotes: 0,
+    },
+    republicans: {
+        name: "Republicans",
+        numberOfMembers: 0,
+        averageVotes: 0,
+    },
+    independents: {
+        name: "Independents",
+        numberOfMembers: 0,
+        averageVotes: 0,
+    },
+    totals: {
+        name: "Total",
+        numberOfMembers: 0,
+        averageVotes: 0,
+    }
 };
 var arrayOfMembers = data.results[0].members;
 
-statistics.numberOfDemocrats = numberOfMembers(arrayOfMembers, "D");
-statistics.numberOfRepublicans = numberOfMembers(arrayOfMembers, "R");
-statistics.numberOfIndependents = numberOfMembers(arrayOfMembers, "I");
-statistics.averageVotesDemocrats = (averageVotes(arrayOfMembers, "D") / statistics.numberOfDemocrats).toFixed(2);
-statistics.averageVotesRepublicans = (averageVotes(arrayOfMembers, "R") / statistics.numberOfRepublicans).toFixed(2);
-statistics.averageVotesIndependents = (averageVotes(arrayOfMembers, "I") / statistics.numberOfIndependents).toFixed(2);
+statistics.democrats.numberOfMembers = numberOfMembers(arrayOfMembers, "D");
+statistics.republicans.numberOfMembers = numberOfMembers(arrayOfMembers, "R");
+statistics.independents.numberOfMembers = numberOfMembers(arrayOfMembers, "I");
+statistics.democrats.averageVotes = (averageVotes(arrayOfMembers, "D") / statistics.democrats.numberOfMembers).toFixed(2);
+statistics.republicans.averageVotes = (averageVotes(arrayOfMembers, "R") / statistics.republicans.numberOfMembers).toFixed(2);
+statistics.independents.averageVotes = (averageVotes(arrayOfMembers, "I") / statistics.independents.numberOfMembers).toFixed(2);
+statistics.totals.numberOfMembers = arrayOfMembers.length;
+statistics.totals.averageVotes = ((Number(statistics.democrats.averageVotes) + Number(statistics.republicans.averageVotes) + Number(statistics.independents.averageVotes)) / 3).toFixed(2);
 console.log(statistics);
+
+createTableGlance(statistics);
 
 function numberOfMembers(members, letter) {
     var numberOfMembersByParty = 0;
@@ -32,4 +53,24 @@ function averageVotes(members, letter) {
         }
     }
     return votesByParty;
+}
+
+function createTableGlance(object) {
+    var tbody = document.getElementById("table-glance");
+
+    for (var keys in statistics) {
+        var tr = document.createElement("tr");
+
+        var td1 = document.createElement("td");
+        var td2 = document.createElement("td");
+        var td3 = document.createElement("td");
+
+        td1.innerHTML = statistics[keys].name;
+        td2.innerHTML = statistics[keys].numberOfMembers;
+        td3.innerHTML = statistics[keys].averageVotes;
+
+        tr.append(td1, td2, td3);
+
+        tbody.append(tr);
+    }
 }
