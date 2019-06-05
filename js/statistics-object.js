53a33fc144605f58888218358e038e25400eb9f8
Simon Criado <Simon@MacBook-Pro-de-Simon.local>
2019-06-05 11:17:48 +0200

    Created function to avoid NaN when calculating averages

--- js/statistics-object.js
+++ js/statistics-object.js
@@ -3,16 +3,19 @@ var statistics = {
         name: "Democrats",
         numberOfMembers: 0,
         averageVotes: 0,
+        missedVotes: 0,
     },
     republicans: {
         name: "Republicans",
         numberOfMembers: 0,
         averageVotes: 0,
+        missedVotes: 0,
     },
     independents: {
         name: "Independents",
         numberOfMembers: 0,
         averageVotes: 0,
+        missedVotes: 0,
     },
     totals: {
         name: "Total",
@@ -22,26 +25,17 @@ var statistics = {
 };
 var arrayOfMembers = data.results[0].members;
 
+
 // Sets total members by party
 statistics.democrats.numberOfMembers = numberOfMembers(arrayOfMembers, "D");
 statistics.republicans.numberOfMembers = numberOfMembers(arrayOfMembers, "R");
 statistics.independents.numberOfMembers = numberOfMembers(arrayOfMembers, "I");
 // Sets average votes by party
-if (averageVotes(arrayOfMembers, "D") !== 0) {
-    statistics.democrats.averageVotes = (averageVotes(arrayOfMembers, "D") / statistics.democrats.numberOfMembers).toFixed(2);
-} else {
-    statistics.democrats.averageVotes = 0;
-}
-if (averageVotes(arrayOfMembers, "R") !== 0) {
-    statistics.republicans.averageVotes = (averageVotes(arrayOfMembers, "R") / statistics.republicans.numberOfMembers).toFixed(2);
-} else {
-    statistics.republicans.averageVotes = 0;
-}
-if (averageVotes(arrayOfMembers, "I") !== 0) {
-    statistics.independents.averageVotes = (averageVotes(arrayOfMembers, "I") / statistics.independents.numberOfMembers).toFixed(2);
-} else {
-    statistics.independents.averageVotes = 0;
-}
+normalizer(arrayOfMembers);
+// Sets average of MISSED votes by party
+statistics.democrats.missedVotes = (missedVotes(arrayOfMembers, "D") / statistics.democrats.numberOfMembers).toFixed(2);
+statistics.republicans.missedVotes = (missedVotes(arrayOfMembers, "R") / statistics.republicans.numberOfMembers).toFixed(2);
+statistics.independents.missedVotes = (missedVotes(arrayOfMembers, "I") / statistics.independents.numberOfMembers).toFixed(2);
 // Sets totals
 statistics.totals.numberOfMembers = arrayOfMembers.length;
 statistics.totals.averageVotes = ((Number(statistics.democrats.averageVotes) + Number(statistics.republicans.averageVotes) + Number(statistics.independents.averageVotes)) / 3).toFixed(2);
@@ -73,6 +67,26 @@ function averageVotes(members, letter) {
     return votesByParty;
 }
 
+// Checks for 0 values to avoid problems with averages
+function normalizer(array) {
+    if (averageVotes(array, "D") !== 0) {
+        statistics.democrats.averageVotes = (averageVotes(array, "D") / statistics.democrats.numberOfMembers).toFixed(2);
+
+    } else {
+        statistics.democrats.averageVotes = 0;
+    }
+    if (averageVotes(array, "R") !== 0) {
+        statistics.republicans.averageVotes = (averageVotes(array, "R") / statistics.republicans.numberOfMembers).toFixed(2);
+
+    } else {
+        statistics.republicans.averageVotes = 0;
+    }
+    if (averageVotes(array, "I") !== 0) {
+        statistics.independents.averageVotes = (averageVotes(array, "I") / statistics.independents.numberOfMembers).toFixed(2);
+    } else {
+        statistics.independents.averageVotes = 0;
+    }
+}
 // Created the table "glance" based on the results above
 function createTableGlance(object) {
     var tbody = document.getElementById("table-glance");
@@ -90,4 +104,15 @@ function createTableGlance(object) {
         tr.append(td1, td2, td3);
         tbody.append(tr);
     }
+}
+
+// Gets the average of the missed votes for each party
+function missedVotes(members, letter) {
+    var missedVotesByParty = 0;
+    for (var i = 0; i < members.length; i++) {
+        if (members[i].party == letter) {
+            missedVotesByParty += (members[i].missed_votes);
+        }
+    }
+    return missedVotesByParty;
 }
