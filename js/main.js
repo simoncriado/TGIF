121e68d4255147bb7c668ad0c7381acd6f80a567
Simon Criado <Simon@MacBook-Pro-de-Simon.local>
2019-06-11 17:24:02 +0200

    Creating filters

--- js/main.js
+++ js/main.js
@@ -1,6 +1,43 @@
 var arrayOfMembers = data.results[0].members;
 
-createTable(arrayOfMembers)
+document.getElementById("republican").addEventListener("click", function () {
+    filteringMembers("R", "republican");
+});
+document.getElementById("democrat").addEventListener("click", function () {
+    filteringMembers("D", "democrat");
+});
+document.getElementById("independent").addEventListener("click", function () {
+    filteringMembers("I", "independent");
+});
+
+// function filteringMembers(members) {
+//     var checkingChecked = getelementbyname("filterMembers");
+//     var checkedArray = [];
+//     for (var i = 0; i < checkingChecked.length; i++) {
+//         var checked = [];
+//         if (checkingChecked[i].checked == true) {
+//             checked.push(checkingChecked[i]);
+//         } else {
+//             break;
+//         }
+//         for (var j = 0; j < members.length; j++) {
+
+//         }
+//     }
+// }
+function filteringMembers(party, id) {
+    var filteredArray = [];
+    for (var i = 0; i < arrayOfMembers.length; i++) {
+        if (arrayOfMembers[i].party == party) {
+            if (document.getElementsById(id).checked == true) {
+                filteredArray.push(arrayOfMembers[i]);
+            } else {
+                filteredArray = arrayOfMembers[i]
+            }
+        }
+    }
+    createTable(filteredArray);
+}
 
 // getMembersName(members);
 function getMembersName(member) {
@@ -16,8 +53,47 @@ function getMembersName(member) {
 }
 
 // Creates the table of members
+// function createTable(members) {
+//     var tbody = document.getElementById("table-body");
+//     tbody.append("");
+
+//     for (var i = 0; i < members.length; i++) {
+//         var tr = document.createElement("tr");
+
+//         var td1 = document.createElement("td");
+//         var td2 = document.createElement("td");
+//         var td3 = document.createElement("td");
+//         var td4 = document.createElement("td");
+//         var td5 = document.createElement("td");
+//         if (members[i].url != "") {
+//             var membersUrl = document.createElement("a");
+//             membersUrl.setAttribute("href", members[i].url);
+
+//             membersUrl.setAttribute("target", "_blank");
+//             membersUrl.innerHTML = getMembersName(members[i]);
+
+//             td1.append(membersUrl);
+//         }
+//         else {
+//             td1.append(getMembersName(members[i]));
+//         }
+//         td2.innerHTML = members[i].party;
+//         td3.innerHTML = members[i].state;
+//         td4.innerHTML = members[i].seniority;
+//         td5.innerHTML = `${members[i].votes_with_party_pct}%`;
+//         tr.append(td1, td2, td3, td4, td5);
+
+//         tbody.append(tr);
+//     }
+// }
+
+// Creates the table of members
+
+
+
 function createTable(members) {
     var tbody = document.getElementById("table-body");
+    tbody.innerHTML = "";
 
     for (var i = 0; i < members.length; i++) {
         var tr = document.createElement("tr");
@@ -39,6 +115,7 @@ function createTable(members) {
         else {
             td1.append(getMembersName(members[i]));
         }
+
         td2.innerHTML = members[i].party;
         td3.innerHTML = members[i].state;
         td4.innerHTML = members[i].seniority;
@@ -64,15 +141,4 @@ function myFunction() {
         btnText.innerHTML = "Read less";
         moreText.style.display = "inline";
     }
-}
-
-// filters("republican", checking());
-// function checking() {
-//     console.log("checking");
-// }
-
-
-function filters(id, someFunction) {
-    document.getElementById(id).addEventListener("click", someFunction);
-    console.log("checking");
 }
